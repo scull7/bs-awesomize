0ba13d2269e06f0060c9a1d5c7398a37709f6ba7
Nathan Sculli <scull7@gmail.com>
2018-05-05 16:47:15 -0700

    do not ignore just javascript files

---
+++ __tests__/TestJavaScriptInterface.js
@@ -0,0 +1,168 @@
+const Awesomize = require('../index');
+
+describe('Awesomize interface to JavaScript', () => {
+  const schema = Awesomize.compile({
+    'foo': {
+      read: ({ foo }) => foo,
+      sanitize: (x) => x + 'bar',
+      validate: [ Awesomize.Validate.required ],
+      normalize: null,
+    },
+    'bar': {
+      read: ({ bar }) => bar,
+      sanitize: null,
+      validate: [ Awesomize.Validate.required ],
+      normalize: null,
+    },
+  });
+  it('should respond with an error type', (done) => {
+    schema({
+      'foo': 'something',
+    })
+      .then((result) => {
+        expect(result.awesomeResultType).toBe('Error');
+        expect(result.data).toBeNull();
+        expect(result.messages.foo).toBeNull();
+        expect(result.messages.bar).toBe('required')
+        done();
+      });
+  });
+  it('should respond with a success type', (done) => {
+    schema({
+      'foo': 'thing1',
+      'bar': 'thing2',
+    })
+      .then((result) => {
+        expect(result.awesomeResultType).toBe('Ok');
+        expect(result.messages).toBeNull();
+        expect(result.data.foo).toBe('thing1bar');
+        expect(result.data.bar).toBe('thing2');
+        done();
+      })
+  });
+});
+
+describe('Awesomize Validator JavaScript.externDependent', () => {
+  const notEqual = (target, dependent, _sanitized) => {
+    if (dependent == null) {
+      return Promise.resolve(false);
+    }
+    return Promise.resolve(target !== dependent);
+  };
+
+  let validator =  Awesomize.Validate.externDependent(
+    notEqual,
+    "bar",
+    "should_not_match"
+  );
+
+  const schema = Awesomize.compile({
+    "foo": {
+      read: ({ foo }) => foo,
+      sanitize: null,
+      validate: [ validator ],
+      normalize: null,
+    },
+    "bar": {
+      read: ({ bar }) => bar,
+      sanitize: null,
+      validate: [ Awesomize.Validate.required ],
+      normalize: null,
+    },
+  });
+
+  it("should fail when the dependent value is not present", (done) => {
+    schema({ foo: 42 })
+
+      .then(result => {
+        expect(result.awesomeResultType).toBe('Error');
+        expect(result.data).toBeNull();
+        expect(result.messages.foo).toBe("should_not_match");
+        expect(result.messages.bar).toBe("required");
+        done();
+      });
+  });
+
+  it("should fail when the dependent validator test fails", (done) => {
+    schema({ foo: 42, bar: 42 })
+
+      .then(result => {
+        expect(result.awesomeResultType).toBe('Error');
+        expect(result.data).toBeNull();
+        expect(result.messages.foo).toBe("should_not_match");
+        expect(result.messages.bar).toBeNull();
+        done();
+      });
+  });
+
+  it("should pass when the dependent validator test passes", (done) => {
+    schema({ foo: 42, bar: 43 })
+
+      .then((result) => {
+        expect(result.awesomeResultType).toBe('Ok');
+        expect(result.messages).toBeNull();
+        expect(result.data.foo).toBe(42);
+        expect(result.data.bar).toBe(43);
+        done();
+      });
+  });
+
+  describe("Synchronous validator", () => {
+    const isEqual = (target, dependent, _sanitized) => (dependent == null)
+      ? false
+      : (target === dependent);
+
+    const schema = Awesomize.compile({
+      "foo": {
+        read: ({ foo }) => foo,
+        sanitize: null,
+        validate: [
+          Awesomize.Validate.externDependent(isEqual, "bar", "must_match"),
+        ],
+        normalize: null,
+      },
+      "bar": {
+        read: ({ bar }) => bar,
+        sanitize: null,
+        validate: [ Awesomize.Validate.required ],
+        normalize: null,
+      },
+    });
+
+    it("should fail when the dependent value is not present", (done) => {
+      schema({ foo: 42 })
+
+        .then(result => {
+          expect(result.awesomeResultType).toBe('Error');
+          expect(result.data).toBeNull();
+          expect(result.messages.foo).toBe("must_match");
+          expect(result.messages.bar).toBe("required");
+          done();
+        });
+    });
+
+    it("should fail when the dependent validator test fails", (done) => {
+      schema({ foo: 42, bar: 43 })
+
+        .then(result => {
+          expect(result.awesomeResultType).toBe('Error');
+          expect(result.data).toBeNull();
+          expect(result.messages.foo).toBe("must_match");
+          expect(result.messages.bar).toBeNull();
+          done();
+        });
+    });
+
+    it("should pass when the dependent validator test passes", (done) => {
+      schema({ foo: 42, bar: 42 })
+
+        .then((result) => {
+          expect(result.awesomeResultType).toBe('Ok');
+          expect(result.messages).toBeNull();
+          expect(result.data.foo).toBe(42);
+          expect(result.data.bar).toBe(42);
+          done();
+        });
+    });
+  });
+});
